5c7871e882d22a2b869b3e0abda3a531789df290
huiruo <2196411859@qq.com>
2023-03-29 18:55:59 +0800

    feat: test file

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -2,16 +2,13 @@ import { withIronSessionSsr } from 'iron-session/next'
 import { sessionOptions } from '@common/session'
 import { Stats } from '@one-chat/api-sdk'
 import { Box } from '@fower/react'
-import { DemoRequest } from '../../demo-request'
 
 interface Props {
   stats: Stats
 }
 
 export default function PageHome({ stats }: Props) {
-  return <Box>home
-    <DemoRequest />
-  </Box>
+  return <Box>home</Box>
 }
 
 export const getServerSideProps = withIronSessionSsr(async function ({ req, res, locale = '' }) {
