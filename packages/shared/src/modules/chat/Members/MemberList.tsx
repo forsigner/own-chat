767f8bd6f4746136d0d2cca0efcf5689cd6032a7
forsigner <forsigner@gmail.com>
2023-04-01 00:20:17 +0800

    feat: can remove member

--- packages/shared/src/modules/chat/Members/MemberList.tsx
+++ packages/shared/src/modules/chat/Members/MemberList.tsx
@@ -1,6 +1,43 @@
 import { Box } from '@fower/react'
-import { Avatar } from 'bone-ui'
+import { Member } from '@own-chat/api-sdk'
+import {
+  Avatar,
+  Button,
+  Popover,
+  PopoverBody,
+  PopoverContent,
+  PopoverTitle,
+  PopoverTrigger,
+  Tag,
+} from 'bone-ui'
 import { useMembers } from '../hooks/useMembers'
+import { useRemoveMember } from '../hooks/useRemoveMember'
+
+function MemberItem({ item, isOwner }: { item: Member; isOwner: boolean }) {
+  const { removeMember } = useRemoveMember()
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Box key={item.id} cursorPointer>
+          <Avatar src={item.user.avatar} />
+        </Box>
+      </PopoverTrigger>
+      <PopoverContent minW-100>
+        <PopoverTitle toCenterX>@{item.user.nickname}</PopoverTitle>
+        <PopoverBody column toCenterX>
+          {isOwner && <Tag colorScheme="brand500">Owner</Tag>}
+          {!isOwner && (
+            <Box>
+              <Button size="sm" colorScheme="red500" onClick={() => removeMember(item.id)}>
+                移除该成员
+              </Button>
+            </Box>
+          )}
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  )
+}
 
 export function MemberList() {
   const { members, loading } = useMembers()
@@ -9,10 +46,8 @@ export function MemberList() {
 
   return (
     <Box toCenterY columnGap-8>
-      {members.map((item) => (
-        <Box key={item.id} cursorPointer>
-          <Avatar src={item.user.avatar} />
-        </Box>
+      {members.map((item, index) => (
+        <MemberItem key={item.id} isOwner={index === 0} item={item} />
       ))}
     </Box>
   )
